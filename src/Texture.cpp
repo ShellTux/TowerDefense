499686fbd652b21de1fd57ddd9c60681e5297f9b
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-11-27 23:38:27 +0000

    feat(Texture): glTexEnvf

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -11,6 +11,8 @@ Texture::Texture(const std::filesystem::path &path)
 {
 	glGenTextures(1, &renderId);
 	glBindTexture(GL_TEXTURE_2D, renderId);
+
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
